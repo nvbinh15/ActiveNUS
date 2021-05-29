e04dec847946784f1d6f4c405d920431f7669530
nvbinh15 <binh.pdc01@gmail.com>
2021-05-29 21:19:01 +0800

    add domain and fix validation email

--- activenus/activenus/settings.py
+++ activenus/activenus/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY',)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', ]
+ALLOWED_HOSTS = ['127.0.0.1', 'activenus.herokuapp.com']
 
 AUTH_USER_MODEL = "authentication.User" 
 
@@ -36,8 +36,9 @@ LOGIN_URL = "auth/login"
 # Application definition
 
 INSTALLED_APPS = [
+    'dashboard',
     'authentication',
-    
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
