8b4c72d6285f5c8af930107698bae6310a6ddddd
nvbinh15 <binh.pdc01@gmail.com>
2021-05-29 21:07:33 +0800

    complete authentication, set up dashboard ui

--- activenus/activenus/urls.py
+++ activenus/activenus/urls.py
@@ -1,4 +1,4 @@
-"""activenus URL Configuration
+"""testauth URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
-]
+    path('', include('dashboard.urls')),
+] 
