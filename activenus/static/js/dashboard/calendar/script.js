c5dd68d1a0db774a43c0a0938376f0799cbf8a3b
nvbinh15 <binh.pdc01@gmail.com>
2021-06-19 01:39:22 +0800

    add flashcard

--- activenus/static/js/dashboard/calendar/script.js
+++ activenus/static/js/dashboard/calendar/script.js
@@ -1,30 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
 
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
-
-	today = yyyy + '-' + mm + '-' + dd;
-
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-      },
-      initialDate: today,
-      navLinks: true, // can click day/week names to navigate views
-      nowIndicator: true,
-
-      weekNumbers: true,
-      weekNumberCalculation: 'ISO',
-
-      editable: true,
-      selectable: true,
-      dayMaxEvents: true, // allow "more" link when too many events
-      events: [
+  var ev;
+    if(localStorage.getItem('ev')){
+      ev = JSON.parse(localStorage.getItem('ev'));
+    } else {
+      ev = [
         {
           title: 'All Day Event',
           start: '2021-06-01'
@@ -79,8 +59,57 @@ document.addEventListener('DOMContentLoaded', function() {
           url: 'http://google.com/',
           start: '2021-06-28'
         }
-      ]
+      ];
+    }
+    var calendarEl = document.getElementById('calendar');
+    var calendarEl = document.getElementById('calendar');
+
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+	today = yyyy + '-' + mm + '-' + dd;
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+      },
+      initialDate: today,
+      navLinks: true, // can click day/week names to navigate views
+      nowIndicator: true,
+
+      weekNumbers: true,
+      weekNumberCalculation: 'ISO',
+
+      editable: true,
+      selectable: true,
+      dayMaxEvents: true, // allow "more" link when too many events
+      events: ev
     });
 
-    calendar.render();
+    document.querySelector('#choose').addEventListener('submit', function(){
+      event.preventDefault();
+      ename = document.getElementById("name").value;
+      estart = document.getElementById("start").value;
+      eend = document.getElementById("end").value;
+      calendar.addEvent({ // this object will be "parsed" into an Event Object
+      title: ename, // a property!
+      start: estart, // a property!
+      end: eend // a property! ** see important note below about 'end' **
+      });
+      console.log(ev);
+      localStorage.setItem('ev', JSON.stringify([{ // this object will be "parsed" into an Event Object
+      title: ename, // a property!
+      start: estart, // a property!
+      end: eend // a property! ** see important note below about 'end' **
+      },...ev]));
+      document.getElementById("name").value = "";
+      document.getElementById("start").value = "";
+      document.getElementById("end").value = "";
   });
+
+  calendar.render();
+});
