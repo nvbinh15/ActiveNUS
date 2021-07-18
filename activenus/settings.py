c6c44e32f764b303367201512ef757807890c753
nvbinh15 <binh.pdc01@gmail.com>
2021-07-18 12:48:29 +0800

    Fix heroku database

--- activenus/settings.py
+++ activenus/settings.py
@@ -146,3 +146,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
