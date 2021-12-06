521fdf7cdbeb316eadc9bdea7f3a3861560716b6
nvbinh15 <binh.pdc01@gmail.com>
2021-12-06 14:50:35 +0800

    Add helpers docstrings

--- helpers/calendar.py
+++ helpers/calendar.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 
 def date_end(cur_date, offset):
+    """Returns the ending date of a task"""
     return cur_date + timedelta(days=offset)
 
 def automatic_schedule(new_prog):
--- helpers/decorators.py
+++ helpers/decorators.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.decorators import user_passes_test
 
 def check_user(user):
+    """Returns true if the user is not authenticated, false otherwise"""
     return not user.is_authenticated
 
 user_logout_required = user_passes_test(check_user, '/', None)
 
 def auth_user_should_not_access(viewfunc):
+    """Returns a decorator restricting logged in user"""
     return user_logout_required(viewfunc)
